5a55948e737bf7f2613556a280d5f59258396453
Sean Coates <sean@seancoates.com>
2025-02-24 02:31:13 +0000

    fileID instead of just file

--- Sources/App/Controllers/PasteController.swift
+++ Sources/App/Controllers/PasteController.swift
@@ -19,7 +19,7 @@ struct PasteController<Context: RequestContext> {
 
         let alreadyHasCookie = cookieSecret == self.secret
         if alreadyHasCookie {
-            logger.info("\(#file):\(#function) Skipping 'remember' UI; request already has a correct cookie.")
+            logger.info("\(#fileID):\(#function) Skipping 'remember' UI; request already has a correct cookie.")
         }
 
         return .init(
@@ -43,9 +43,9 @@ struct PasteController<Context: RequestContext> {
         let input: SubmissionInput
         do {
             input = try await request.decode(as: SubmissionInput.self, context: context)
-            logger.info("\(#file):\(#function) Received valid input.")
+            logger.info("\(#fileID):\(#function) Received valid input.")
         } catch {
-            logger.warning("\(#file):\(#function) Received invalid input.")
+            logger.warning("\(#fileID):\(#function) Received invalid input.")
             redirect.headers[.location] = "\($Routing.getRoot.path)?err=input"
             return redirect
         }
@@ -55,7 +55,7 @@ struct PasteController<Context: RequestContext> {
             ||
             request.cookies[self.cookieName]?.value == self.secret
         else {
-            logger.warning("\(#file):\(#function) Secret is incorrect.")
+            logger.warning("\(#fileID):\(#function) Secret is incorrect.")
             redirect.setCookie(.init(name: self.cookieName, value: "", httpOnly: true))
             redirect.headers[.location] = "\($Routing.getRoot.path)?err=secret"
             return redirect
@@ -68,17 +68,17 @@ struct PasteController<Context: RequestContext> {
         let key: String
         do {
             key = try await storage.put(paste: input.paste)
-            logger.info("\(#file):\(#function) Wrote to key: \(key)")
+            logger.info("\(#fileID):\(#function) Wrote to key: \(key)")
         } catch {
             // failed to write
-            logger.warning("\(#file):\(#function) Could not write to storage. Error: \(error)")
+            logger.warning("\(#fileID):\(#function) Could not write to storage. Error: \(error)")
             return .init(status: .internalServerError, body: .init(byteBuffer: .init(string: "Could not write to storage.")))
         }
 
         if setCookie {
             // we store this as plaintext; it's low-value
             // if we get here, the user has authenticated so we can use self.secret
-            logger.info("\(#file):\(#function) Setting the secret cookie.")
+            logger.info("\(#fileID):\(#function) Setting the secret cookie.")
             redirect.setCookie(.init(name: self.cookieName, value: self.secret, maxAge: 31536000, httpOnly: true))
         }
 
@@ -90,13 +90,13 @@ struct PasteController<Context: RequestContext> {
     @Sendable func getPaste(request: Request, context: Context) async throws -> TemplResponse {
 
         guard let key = context.parameters.get("key") else {
-            logger.warning("\(#file):\(#function) Could not get key from request.")
+            logger.warning("\(#fileID):\(#function) Could not get key from request.")
             throw HTTPError(.badRequest)
         }
         do {
-            logger.info("\(#file):\(#function) Fetching key: \(key)")
+            logger.info("\(#fileID):\(#function) Fetching key: \(key)")
             let body = try await storage.get(key: key)
-            logger.info("\(#file):\(#function) Fetched key: \(key)")
+            logger.info("\(#fileID):\(#function) Fetched key: \(key)")
             return .init(
                 status: .ok,
                 headers: [.contentType: "text/plain"],
@@ -104,7 +104,7 @@ struct PasteController<Context: RequestContext> {
             )
         } catch {
             // did not fetch key
-            logger.warning("\(#file):\(#function) Could not fetch key: \(error)")
+            logger.warning("\(#fileID):\(#function) Could not fetch key: \(error)")
             return .init(status: .notFound)
         }
     }
